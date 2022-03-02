f70e503b91634a493f91344f8668a99efb5e7606
CK-Kumar <chandru0063@gmail.com>
2022-03-03 00:57:38 +0100

    Image upload to firebase

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/resources/storage_methods.dart';
+import 'package:instagram_clone/resources/storage_methods.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -15,7 +17,7 @@ class AuthMethods {
     required String password,
     required String username,
     required String bio,
-    //required Uint8List file,
+    required Uint8List file,
   }) async {
     String res = 'Some error occurred';
     try {
@@ -26,6 +28,8 @@ class AuthMethods {
         //resgister user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
+        String photoUrl = await StorageMethods()
+            .uploadImageToStorage('ProfilePic', file, false);
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': username,
           'uid': cred.user!.uid,
@@ -33,6 +37,7 @@ class AuthMethods {
           'bio': bio,
           'followers': [],
           'following': [],
+          'photoUrl': photoUrl
         });
         res = 'Success';
       }
