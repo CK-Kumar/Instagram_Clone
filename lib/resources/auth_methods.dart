0047615941c5963f7a51064195ef1d803f2a286b
CK-Kumar <chandru0063@gmail.com>
2022-03-08 00:35:17 +0100

    UserProvider created

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/user.dart' as model;
 import 'package:instagram_clone/resources/storage_methods.dart';
 import 'package:instagram_clone/resources/storage_methods.dart';
 
@@ -30,15 +31,18 @@ class AuthMethods {
             email: email, password: password);
         String photoUrl = await StorageMethods()
             .uploadImageToStorage('ProfilePic', file, false);
-        await _firestore.collection('users').doc(cred.user!.uid).set({
-          'username': username,
-          'uid': cred.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl
-        });
+
+        model.User user = model.User(
+            userName: username,
+            uid: cred.user!.uid,
+            email: email,
+            bio: bio,
+            photoUrl: photoUrl,
+            followers: [],
+            following: []);
+        await _firestore.collection('users').doc(cred.user!.uid).set(
+              user.toJson(),
+            );
         res = 'Success';
       }
     } catch (e) {
