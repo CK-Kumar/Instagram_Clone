7e1e761dcc7a7fd7e639c996ea33be30d96023a5
CK-Kumar <chandru0063@gmail.com>
2022-03-24 00:22:17 +0100

    add post ui

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -45,9 +45,35 @@ class _AddPostScreenState extends State<AddPostScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 backgroundImage: NetworkImage(
                     'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ed991cf4-7c8c-4530-b6ba-a3abf3ab2eae/dcl6qui-b8412a3c-115f-47c6-b40e-2b2fd77eb0f8.png/v1/fill/w_600,h_736,strp/super_mario__mario_icon_2d_by_joshuat1306_dcl6qui-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzM2IiwicGF0aCI6IlwvZlwvZWQ5OTFjZjQtN2M4Yy00NTMwLWI2YmEtYTNhYmYzYWIyZWFlXC9kY2w2cXVpLWI4NDEyYTNjLTExNWYtNDdjNi1iNDBlLTJiMmZkNzdlYjBmOC5wbmciLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.pdnNdKyYUawtgJZ7q4TnSxT3N3h4VzKZa_22--POa-M'),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.3,
+                child: const TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Write a caption...', border: InputBorder.none),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                width: 45,
+                height: 45,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ed991cf4-7c8c-4530-b6ba-a3abf3ab2eae/dcl6qui-b8412a3c-115f-47c6-b40e-2b2fd77eb0f8.png/v1/fill/w_600,h_736,strp/super_mario__mario_icon_2d_by_joshuat1306_dcl6qui-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzM2IiwicGF0aCI6IlwvZlwvZWQ5OTFjZjQtN2M4Yy00NTMwLWI2YmEtYTNhYmYzYWIyZWFlXC9kY2w2cXVpLWI4NDEyYTNjLTExNWYtNDdjNi1iNDBlLTJiMmZkNzdlYjBmOC5wbmciLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.pdnNdKyYUawtgJZ7q4TnSxT3N3h4VzKZa_22--POa-M',
+                        ),
+                        fit: BoxFit.fill,
+                        alignment: FractionalOffset.topCenter,
+                      ),
+                    ),
+                  ),
+                ),
               )
             ],
           )
